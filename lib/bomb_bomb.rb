d942edf6456fb6e62ee56de19a828cc1185a2487
Guille Carlos <guille@bitpop.in>
2013-06-18 13:40:10 -0400

    Change to use net http and completed some test for sample calls

--- lib/bomb_bomb.rb
+++ lib/bomb_bomb.rb
@@ -1,5 +1,43 @@
 require "bomb_bomb/version"
+require "net/http"
+require "json"
 
 module BombBomb
 
+  class API
+    def initialize(email, pw)
+      @email = email
+      @pw    = pw
+    end
+
+    def call(method, args)
+      parsed_response = make_call(method, args)
+      parsed_response
+    end
+
+    def method_missing(method, *args)
+      method = camelize_method(method)
+      call(method, args)
+    end
+
+    private
+    def param_fields(args)
+      params          = {}
+      params["email"] = @email
+      params["pw"]    = @pw
+      params          = args.first.merge(params) if args.first
+      params
+    end
+
+    def make_call(method, args)
+      uri = URI("https://app.bombbomb.com/app/api/api.php?method=#{method}")
+      response = Net::HTTP.post_form(uri, param_fields(args))
+      JSON.load("[#{response.body}]").first
+    end
+
+    def camelize_method(method)
+      method = method.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      testmethod = method[0].chr.upcase + method[1..-1]
+    end
+  end
 end
--- test/bomb_bomb_test.rb
+++ test/bomb_bomb_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
+require "bomb_bomb"
+require "minitest/mock"
 
-describe "BombBomb API" do
-  it "Should be true" do
-    true
+describe "BombBomb API method calls samples" do
+  before do
+    @email         = "guille@fivestreet.com"
+    @pw            = "12345"
+    @bomb          = BombBomb::API.new(@email, @pw)
+
+    #Fake Response
+    FakeResponse   = Struct.new(:body)
+    @response      = FakeResponse.new("{}")
+    @mock          = MiniTest::Mock.new
+    @mock.expect :body, @response.body
   end
+
+  it "checks if login is valid" do
+
+    Net::HTTP.stub(:post_form, @mock) do
+      @bomb.is_valid_login
+    end
+
+    assert @mock.verify
+  end
+
+  it "gets all lists" do
+    Net::HTTP.stub(:post_form, @mock) do
+      @bomb.get_lists
+    end
+
+    assert @mock.verify
+  end
+
+  it "gets contacts of a list" do
+    Net::HTTP.stub(:post_form, @mock) do
+      @bomb.get_list_contacts( :list_id => "idtolist")
+    end
+
+    assert @mock.verify
+  end
+
 end
