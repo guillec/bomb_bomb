687971e2cd556091741e67425ea20c494f6f97d2
Guille Carlos <guille@bitpop.in>
2013-06-25 13:05:57 -0400

    Make test pass

--- test/bomb_bomb_test.rb
+++ test/bomb_bomb_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 require "bomb_bomb"
+require "curb"
 require "minitest/mock"
 
 describe "BombBomb API method calls samples" do
@@ -10,15 +11,15 @@ describe "BombBomb API method calls samples" do
     @bomb          = BombBomb::API.new(@email, @pw)
 
     #Fake Response
-    FakeResponse   = Struct.new(:body)
+    FakeResponse   = Struct.new(:body_str)
     @response      = FakeResponse.new("{}")
     @mock          = MiniTest::Mock.new
-    @mock.expect :body, @response.body
+    @mock.expect :body_str, @response.body_str
   end
 
   it "checks if login is valid" do
 
-    Net::HTTP.stub(:post_form, @mock) do
+    Curl.stub(:post, @mock) do
       @bomb.is_valid_login
     end
 
@@ -26,7 +27,7 @@ describe "BombBomb API method calls samples" do
   end
 
   it "gets all lists" do
-    Net::HTTP.stub(:post_form, @mock) do
+    Curl.stub(:post, @mock) do
       @bomb.get_lists
     end
 
@@ -34,7 +35,7 @@ describe "BombBomb API method calls samples" do
   end
 
   it "gets contacts of a list" do
-    Net::HTTP.stub(:post_form, @mock) do
+    Curl.stub(:post, @mock) do
       @bomb.get_list_contacts( :list_id => "idtolist")
     end
 
